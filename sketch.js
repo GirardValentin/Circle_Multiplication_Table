90c9026b5f670ec479247540f6505f4cab315f49
GirardValentin <50149344+GirardValentin@users.noreply.github.com>
2019-06-22 22:44:01 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+// Valentin Girard
+// 21:15 17/06/2019
+// Visualization of multiplication table
+// Press > to see next multiplication table
+//       < to see previous
+
+raduis = 150;
+points = 200;
+timestep = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function mousePressed(){
+  save(timestep + ".jpg");
+}
+
+function keyPressed(){
+  if (keyCode == LEFT_ARROW){
+    timestep -= 1;
+  } else if (keyCode == RIGHT_ARROW){
+    timestep += 1;  
+  }
+}
+
+function draw() {
+  background(0);
+  translate(width/2,height/2);
+  
+  stroke(255,127,0);
+  circle(0,0,r*2);
+  
+  beginShape();
+  for (a = 0; a < TWO_PI; a+= TWO_PI/points){
+    noFill();
+    strokeWeight(0.5);
+    x = cos(a) * r;
+    y = sin(a) * r;
+    vertex(x,y);
+    newx = cos(a*timestep) * r;
+    newy = sin(a*timestep) * r;
+    line(x,y,newx,newy);
+  }
+  endShape(CLOSE);
+  
+  text("Multiplication Table of : " + timestep,-60,180);
+
+}
